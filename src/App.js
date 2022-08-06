9b8c0210d49cb9109dcfc0ae5470d43a5c0eb578
altinawiwaseem <wasemm@live.com>
2022-08-06 14:08:48 +0200

    fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
-
+import Row from './components/Row';
+import requests from './requests';
 function App() {
+
+
+
+useEffect(()=>{
+  
+},[])
+
+
   return (
+
     <div className="App">
-      
+      <Row title= "NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title= "Trending now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
