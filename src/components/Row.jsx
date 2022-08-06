bf164d4473b1304933d6be53b6f2cded09fdd972
altinawiwaseem <wasemm@live.com>
2022-08-06 14:32:22 +0200

    fetching images

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,30 +1,32 @@
 import React, { useState, useEffect } from "react";
-import axios from "../fetch"
+import axios from "../fetch";
+
+/* img base url */
+const baseUrl = "https://image.tmdb.org/t/p/w300/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
-const a = 5
-console.log(a) 
+  const a = 5;
+  console.log(a);
 
-
-   useEffect(()=>{
-async function fetchData (){
-const request = await axios.get(fetchUrl)
-console.log("request",request.data.results)
-setMovies(request.data.results)
-return request
-}
-fetchData();
-   },[fetchUrl])
-
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log("request", request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
   return (
     <div className="row">
-      <h2>{title }</h2>
+      <h2>{title}</h2>
       <div className="row_posters"></div>
       {/* several row_posters */}
-      {movies.map(item => item.name)}
-
+      {movies.map((movie) => (
+        <img src={baseUrl + movie.backdrop_path} alt={movie.name} />
+      ))}
     </div>
   );
 }
