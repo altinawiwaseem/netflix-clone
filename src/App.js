388413f706b4613a2a476521ff87933906315933
altinawiwaseem <wasemm@live.com>
2022-08-20 17:10:33 +0200

    adding youtube trailer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import { useEffect } from 'react';
-import './App.css';
-import Row from './components/Row';
-import requests from './requests';
-import Banner from './components/Banner';
-import Navbar from './components/Navbar';
+import { useEffect } from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
 function App() {
-
-
-
-useEffect(()=>{
-  
-},[])
-
+  useEffect(() => {}, []);
 
   return (
-
     <div className="app">
       {/* Nav */}
-    <Navbar/>
-
+      <Navbar />
       {/* Banner */}
-
-      <Banner/>
-
-
-      <Row title= "NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-      <Row title= "Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title= "Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title= "Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title= "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title= "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title= "Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title= "Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />} />
     </div>
   );
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,36 +1,68 @@
 import React, { useState, useEffect } from "react";
 import axios from "../fetch";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 /* img base url */
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, setTrailerUrl] = useState("");
+  console.log("mo", movies);
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl);
-      console.log("request", request.data.results);
-      setMovies(request.data.results);
-      return request;
+      try {
+        const request = await axios.get(fetchUrl);
+        setMovies(request.data.results);
+        return request;
+      } catch (error) {
+        console.log(error);
+      }
     }
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row_posters" >
+      <div className="row_posters">
         {/* several row_posters */}
         {movies.map((movie) => (
           <img
-          key={movie.id}
+            onClick={() => handleClick(movie)}
+            key={movie.id}
             className={`row_poster ${isLargeRow && "row_posterLarge "}`}
-            src={` ${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path }`  }
+            src={` ${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
